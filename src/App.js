86c1f555bcb0ad03a26f0462cd59f417c56ec5b2
Garrett McMillan <cgarrettmcmillan@gmail.com>
2020-04-22 18:06:52 -0400

    general wireframe

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import Btn from './components/Btn';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="calc-wrapper">
-        <div className="row">
-        <Btn>7</Btn>
-        <Btn>8</Btn>
-        <Btn>9</Btn>
-        <Btn>/</Btn>
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="calc-wrapper">
+          <div className="row">
+            <Btn>7</Btn>
+            <Btn>8</Btn>
+            <Btn>9</Btn>
+            <Btn>/</Btn>
+          </div>
+          <div className="row">
+            <Btn>4</Btn>
+            <Btn>5</Btn>
+            <Btn>6</Btn>
+            <Btn>*</Btn>
+          </div>
+          <div className="row">
+            <Btn>1</Btn>
+            <Btn>2</Btn>
+            <Btn>3</Btn>
+            <Btn>+</Btn>
+          </div>
+          <div className="row">
+            <Btn>.</Btn>
+            <Btn>0</Btn>
+            <Btn>=</Btn>
+            <Btn>-</Btn>
+          </div>
         </div>
-
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,36 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const isOperator = val => {
-   return !isNaN(val) || val === '.' || val === '=';
+import './Btn.css';
+
+class Btn extends Component {
+   render() {
+      return (
+         <div className="app">
+         
+         </div>
+      );
+   }
 }
 
-export default function Btn(props) { 
-   return (
-      <div 
-         className={`button-wrapper ${
-            isOperator(props.children) ? null : 'operator'}`} 
-         style={btnStyle}
-      >
-         {props.children}
-      </div> 
-   )
-}
-
-const btnStyle = {
-   height: '4em',
-   display: 'flex',
-   justifyContent: 'center',
-   alignItems: 'center',
-   fontWeight: 'lighter',
-   fontSize: '1.4em',
-   backgroundColor: '#e0e1e6',
-   color: '#888',
-   flex: '1',
-   outline: '1px solid #888',
-}
-
-const operatorStyle = {
-   backgroundColor: '#fe9241',
-   color: 'white',
-}
-
+export default Btn;
