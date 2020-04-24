9256bdc4dba9aab1c75db6e4223837ccc1a15c8c
Garrett McMillan <cgarrettmcmillan@gmail.com>
2020-04-23 21:03:13 -0400

    finished mathjs functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Btn from './components/Btn';
 import Input from './components/Input';
 import ClearBtn from './components/ClearBtn';
+import * as math from 'mathjs';
+
 import './App.css';
 
 
@@ -37,42 +39,56 @@ class App extends Component {
     this.setState({ input: "" });
   };
 
+  handleEqual = () => {
+    this.setState({ input: math.evaluate(this.state.input) })
+  };
+
+  handleNegative = val => {
+    if (this.state.input > 0) {
+      this.setState({ input: this.state.input + math.evaluate(val * -1)
+      })
+    }
+  }
+  
+  
   render() {
     return (
-      <div className="app">
-        <div className="calc-wrapper">
-          <div className="row">
-            <Input>{this.state.input}</Input>
-          </div>
-          <div className="row">
-            <Btn handleClick={this.addToInput} >7</Btn>
-            <Btn handleClick={this.addToInput} >8</Btn>
-            <Btn handleClick={this.addToInput} >9</Btn>
-            <Btn  >/</Btn>
-          </div>
-          <div className="row">
-            <Btn handleClick={this.addToInput} >4</Btn>
-            <Btn handleClick={this.addToInput} >5</Btn>
-            <Btn handleClick={this.addToInput} >6</Btn>
-            <Btn >*</Btn>
-          </div>
-          <div className="row">
-            <Btn handleClick={this.addToInput} >1</Btn>
-            <Btn handleClick={this.addToInput} >2</Btn>
-            <Btn handleClick={this.addToInput} >3</Btn>
-            <Btn >+</Btn>
-          </div>
-          <div className="row">
-            <Btn  handleClick={this.addDecimal} >.</Btn>
-            <Btn  handleClick={this.addZeroToInput} >0</Btn>
-            <Btn >=</Btn>
-            <Btn >-</Btn>
-          </div>
-          <div className="row">
-            <ClearBtn handleClear={this.clearInput}>Clear</ClearBtn>
+        <div className="app">
+          <div className="calc-wrapper">
+            <div className="row">
+              <Input>{this.state.input}</Input>
+            </div>
+            <div className="row" style={{marginTop: '10px'}}>
+              <ClearBtn handleClear={this.clearInput} >WIPE CLEAN</ClearBtn>
+              {/* <Btn handleClick={this.handleNegative} >-/+</Btn>
+              <Btn handleClick={this.addToInput} >%</Btn> */}
+            </div>
+            <div className="row" style={{marginTop: '5px'}}>
+              <Btn handleClick={this.addToInput} >7</Btn>
+              <Btn handleClick={this.addToInput} >8</Btn>
+              <Btn handleClick={this.addToInput} >9</Btn>
+              <Btn handleClick={this.addToInput} >/</Btn>d
+            </div>
+            <div className="row">
+              <Btn handleClick={this.addToInput} >4</Btn>
+              <Btn handleClick={this.addToInput} >5</Btn>
+              <Btn handleClick={this.addToInput} >6</Btn>
+              <Btn handleClick={this.addToInput} >*</Btn>
+            </div>
+            <div className="row">
+              <Btn handleClick={this.addToInput} >1</Btn>
+              <Btn handleClick={this.addToInput} >2</Btn>
+              <Btn handleClick={this.addToInput} >3</Btn>
+              <Btn handleClick={this.addToInput} >+</Btn>
+            </div>
+            <div className="row">
+              <Btn handleClick={this.addDecimal} >.</Btn>
+              <Btn handleClick={this.addZeroToInput} >0</Btn>
+              <Btn handleClick={this.handleEqual} >=</Btn>
+              <Btn handleClick={this.addToInput} >-</Btn>
+            </div>
           </div>
         </div>
-      </div>
     );
   }
 }
